fb3777fdd27df22af7e8025b641a6a4333a1f6ae
Ayush Kumar Dhingra <ayushkumardhingra@gmail.com>
2026-02-21 21:56:48 +0530

    21-02-2026

---
+++ Dart/04_constants.dart
@@ -0,0 +1,21 @@
+
+// ignore_for_file: unused_local_variable
+
+void main() {
+
+	// final
+	final cityName = 'Mumbai';
+	//	name = 'Peter';     // Throws an error
+
+	final String countryName = 'India';
+
+	// const
+	const PI = 3.14;
+	const double gravity = 9.8;
+}
+
+class Circle {
+
+	final color = 'Red';
+	static const PI = 3.14;
+}
