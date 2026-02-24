96b782b2ba9c6015b1632b6425044323dd4b7e22
Ayush Kumar Dhingra <ayushkumardhingra@gmail.com>
2026-02-24 22:24:08 +0530

    24-02-2026

---
+++ Dart/11_break_keyword.dart
@@ -0,0 +1,20 @@
+
+// ignore_for_file: unused_label
+
+void main() {
+
+	// BREAK keyword
+	// Using Labels
+	// Nested FOR Loop
+
+	myOuterLoop: for (int i = 1; i <= 3; i++) {
+
+		innerLoop: for (int j = 1; j <= 3; j++) {
+			print("$i $j");
+
+			if (i == 2 && j == 2) {
+				break myOuterLoop;
+			}
+		}
+	}
+}
